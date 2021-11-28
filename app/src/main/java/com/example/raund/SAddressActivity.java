95698630d5007bf97b846dc9b2c4b7a62fcd58b8
dhindakiflia <dhindakeep@gmail.com>
2021-11-28 17:58:45 +0700

    update

---
+++ app/src/main/java/com/example/raund/SAddressActivity.java
@@ -0,0 +1,25 @@
+package com.example.raund;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SAddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s_address);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+    @Override
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return true;
+    }
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+    }
+}
